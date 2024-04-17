26f4219b72e0acf629770d9010147fe2649f5f08
KorsahS <stefano.korsah@cortislentini.it>
2024-04-17 13:03:18 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+define('SURNAME','LORT');//const
+$name = 'Steeve';
+$age = 24;
+//SURNAME = 'SSSS'; errore
+//echo '<b>Hello world</b>';
+//echo '<br>'
+//echo '<b>Anita MAx Win</b>';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>my first PHP file</title>
+</head>
+<body>
+    <h1><?php echo 'Shut your ass up and do some drums'; ?></h1>
+    <div><?php echo $name ?></div>
+    <div><?php echo $age ?></div>
+    <div><?php echo SURNAME ?></div>
+</body>
+</html>
