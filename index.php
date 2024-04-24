a4928b2c261a93ca2d8193d065ff37cf50ca83a8
KorsahS <stefano.korsah@cortislentini.it>
2024-04-24 16:59:26 +0200

    Associative array finished

--- index.php
+++ index.php
@@ -100,7 +100,7 @@ $intigers = [20, 30, 54, 72, 89];
 $intigers[1] = 35;
 $intigers[] = 61;
 array_push($people, 'gino');
-$stringArray = array_merge($people, $animals);
+$mergedArray = array_merge($people, $animals);
 
 //echo $people;//error arrays are difficult to automaticaly convert to strings
 echo $people[2];
@@ -111,10 +111,27 @@ print_r($animals);
 echo '<br>';
 echo count($intigers);
 echo '<br>';
-print_r($stringArray);
+print_r($mergedArray);
+echo '<br>';
+echo '<br>';
 
-//associative
+//associative (key & value pair)
+$assocArray = ['one' => 'uno', 'two'=>'due', 'three' => 'tre','four'=>'quattro'];
+$test = array('dog' => 'cane', 'cat' => 'gatto', 'cow' => 'mucca');
 
+$assocArray['five'] = 'cinque';
+$assocArray['two'] = 'otto';
+
+$merge = array_merge($assocArray, $test);
+
+echo count($assocArray);
+echo '<br>';
+print_r($assocArray);
+echo '<br>';
+echo $test['dog'];
+echo '<br>';
+print_r($merge);
+echo '<br>';
 
 
 
